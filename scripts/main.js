5f42ea300fd45c75f1d83fd70a106641399b7168
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-12-19 23:26:49 +0300

    week-1

--- scripts/main.js
+++ scripts/main.js
@@ -1,105 +1,97 @@
-'use strict';
-
+let file = null;
+const publishButton = document.querySelector('#post-publish');
 const postModal = document.querySelector('.add-post-modal');
 const buttonAddPhoto = document.querySelector('#add-photo');
 const buttonFirstPost = document.querySelector('#add-first-post');
 const bodyOverlay = document.querySelector('.body-overlay');
-
-//Добавила обработчик на 2 кнопки для открытия модального окна, также добавила классы для body и bodyOverlay для запрета прокрутки при открытии модалки
-buttonFirstPost.addEventListener('click', () => {
-    postModal.classList.add('active');
-    document.body.classList.add('with-overlay');
-    bodyOverlay.classList.add('active');
-})
-
-buttonAddPhoto.addEventListener('click', () => {
-    postModal.classList.add('active');
-    document.body.classList.add('with-overlay');
-    bodyOverlay.classList.add('active');
-})
-
-//Добавление фото
-const loadPhoto = () => {
+const postText = document.querySelector('#post-text');
+const hashtags = document.querySelector('#post-hashtags');
 const fileUpload = document.querySelector('#file-upload');
-const stepOne = document.querySelector('.add-post-modal__step-1'); 
-const stepTwo = document.querySelector('.add-post-modal__step-2'); 
+const stepOne = document.querySelector('.add-post-modal__step-1');
+const stepTwo = document.querySelector('.add-post-modal__step-2');
 const modalFooter = document.querySelector('.modal__footer');
-
-  fileUpload.addEventListener('change', () => {
-      image.src = URL.createObjectURL(fileUpload.files[0]);
-      image.style.display = "block";
-
-      if (fileUpload) {
-          console.log(fileUpload);
-          stepOne.classList.add('hidden');
-          stepTwo.classList.remove('hidden');
-          stepTwo.classList.add('active');
-          modalFooter.classList.remove('hidden');
-      } else {
-          console.log('Ошибка');
-      }
-  });
-};
-
-loadPhoto();
+const successMessage = document.querySelector('#alert-success');
+const errorMessage = document.querySelector('#alert-fail');
 
 
-//Добавила ввод текста в форме, ввод текста в textarea и сохранение в контейнер
-const textarea = document.querySelector('textarea');
-const textContainer = document.querySelector('.add-post-modal__step-2')
-textContainer.classList.remove('hidden');//открыли шаг 2, написание текста в форму
-textarea.addEventListener('keydown', function () {
+buttonAddPhoto.addEventListener('click', addPost);
+buttonFirstPost.addEventListener('click', addPost);
 
-  const messageText = textarea.value;
-  const newMessage = document.createElement('div');
-  newMessage.textContent = messageText;
+function addPost() {
+  postModal.classList.add('active');
+  document.body.classList.add('with-overlay');
+  bodyOverlay.classList.add('active');
+}
 
-  textContainer.append(newMessage);
-  textarea.value = '';
+//Добавление фото, не могу разобраться почему оно у меня заблокировано и дальше не пропускает
+function uploadPhoto() {
 
+fileUpload.addEventListener('change', () => {
+  file = fileUpload.files[0];
+  image.src = URL.createObjectURL(file);
+
+  if (fileUpload) {
+    stepOne.classList.add('hidden');
+    stepTwo.classList.remove('hidden');
+    stepTwo.classList.add('active');
+    modalFooter.classList.remove('hidden');
+    console.log(fileUpload);
+  } else {
+    console.log('Ошибка');
+  }
 });
 
-//Добавила post запрос с обработкой ошибок и показом уведомлений(в целом думаю все должно быть верно)
-postModal.onsubmit = async (e) => {
-  e.preventDefault();
+};
 
-  let response = await fetch('https://c-gallery.polinashneider.space/api/v1/posts/', {
+function showSuccessMessage () {
+
+  setTimeout(() => {
+      successMessage.remove();
+  }, 2000);
+};
+
+function showErrorMessage () {
+
+  setTimeout(() => {
+      errorMessage.remove();
+  }, 2000);
+}
+
+//Добавила post запрос с обработкой ошибок и показом уведомлений
+publishButton.addEventListener('click', () => {
+  const formData = new FormData();
+  formData.append('text', postText.value);
+  formData.append('image', file);
+  formData.append('tags', hashtags.value)
+
+  fetch('https://c-gallery.polinashneider.space/api/v1/posts/', {
     method: 'POST',
+    body: formData,
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer: Belskaya20'
+      Authorization: 'Bearerconst AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc1NjgxNDg4LCJpYXQiOjE2NzA4NDMwODgsImp0aSI6ImQ4OTZlMGI3OGU5MDQ3NWRhZDRhZTI3MWUwYzdiY2FjIiwidXNlcl9pZCI6MjN9.KrsnNhRIOHdKTqm3Pr_eZUJRT4zwkfCTrdQFsVJa1Gg'
     },
-    body: new FormData(postModal)({
-      text: 'string',
-      image: 'string',
-      created_at: '2022-12-16T20:43:26.272Z',
-      tags: []
-    })
   })
 
 
     .then((result) => {
       if (result.ok) {
-        setTimeout(() => {
-          alert('Успешно добавлено!');
-          postModal.reset();
-        }, 2000);
+          alert('Успешно!');
+          postModal.classList.remove('active');
       }
     })
-    .then((data) => {
-      console.log(data);
-    })
     .catch((error) => {
-      setTimeout(() => {
-        alert('Ошибка, попробуйте еще раз!');
-        postModal.reset();
-      }, 2000);
+        alert('Ошибка');
     })
-};
+    .finally(() => {
+      fileUpload.value = '';
+      hashtags.value = '';
+      postText.value = '';
+      image.src = '';
+    })
+})
 
 
-//Попробовала сделать закрытие модального окна при нажатии на внешнюю область
+//Закрытие модального окна при нажатии на внешнюю область, в чем здесь может быть ошибка?
 postModal.onmousedown = function (post) {
   let target = post.target;
   let modalContent = postModal.getElementsByClassName('modal__content')[0];
@@ -107,4 +99,3 @@ postModal.onmousedown = function (post) {
     this.classList.remove('.active');
   }
 }
-console.log();
